3b409e3d119a2a534a4e2f852b3fff08c7e69f27
Revathi Kannan <37206283+RevathiKannanGithub@users.noreply.github.com>
2023-10-21 11:39:32 +0530

    Update 19Oct_Capitalize.py

--- 19Oct_Capitalize.py
+++ 19Oct_Capitalize.py
@@ -40,3 +40,21 @@ def solve(name):
             temp[0] = temp[0].upper()
         res.append("".join(temp))
     return "".join(res)
+#
+#####################################################################################################################################################
+#
+import re
+
+def solve(name):
+    return ' '.join(word.capitalize() for word in s.split(' '))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
