323374a63527ea53bb9aea9bee08a3b7aadb4e5d
Revathi Kannan <37206283+RevathiKannanGithub@users.noreply.github.com>
2023-10-23 08:44:03 +0530

    Create 23Oct_ItertoolsProduct.py

---
+++ 23Oct_ItertoolsProduct.py
@@ -0,0 +1,17 @@
+#
+# https://www.hackerrank.com/challenges/itertools-product/problem
+#
+#####################################################################################################################################################
+#                                                            PROBLEM DESCRIPTION                                                                    #
+#####################################################################################################################################################
+#
+from itertools import product
+
+if __name__ == '__main__':
+    line1 = line2 = []
+    line1 = [int(x) for x in input().split(' ')]
+    line2 = [int(y) for y in input().split(' ')]
+    result = []
+    
+    output = list(product(line1, line2))
+    print (' '.join(str(x) for x in output))
